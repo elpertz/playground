4950f9a9e4a45763ef7baaa8a98cfda7efd76325
elpertz <83992673+elpertz@users.noreply.github.com>
2025-03-04 08:44:05 -0500

    Implementación inicial de tarjetas con animaciones de Framer Motion

---
+++ app/components/card/Card.tsx
@@ -0,0 +1,70 @@
+"use client";
+import React, { useRef } from "react";
+import Image from "next/image";
+import { useTransform, motion, useScroll } from "motion/react";
+
+export default function index({
+  index,
+  title,
+  description,
+  src,
+  color,
+  range,
+  progress,
+  targetScale,
+  rotate,
+}: {
+  index: number;
+  title: string;
+  description: string;
+  src: string;
+  link?: string;
+  color: string;
+  range?: number[];
+  targetScale?: number;
+  progress?: any;
+  rotate?: number;
+}) {
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "start start"],
+  });
+  const cardRotate = useTransform(scrollYProgress, [0, 1], [-12, 0]);
+  const cardScale = useTransform(progress, range || [0, 1], [
+    1,
+    targetScale || 1.2,
+  ]);
+  return (
+    <div
+      ref={container}
+      className="flex  items-center justify-center h-svh sticky top-0"
+    >
+      <motion.div
+        style={{
+          scale: cardScale,
+          rotate: cardRotate,
+          backgroundColor: color,
+          top: `calc(-10% + ${index * 25}px)`,
+        }}
+        className="flex  relative h-[500px] w-[1000px] rounded-3xl p-11 origin-top gap-12 items-center overflow-hidden"
+      >
+        <div className="flex flex-col w-2/5 space-y-4 text-balance">
+          <h2 className="text-4xl font-bold">{title}</h2>
+          <div className="my-1">
+            <p>{description}</p>
+          </div>
+        </div>
+        <div className="flex flex-col w-3/5 aspect-square  rounded-3xl relative">
+          <Image
+            src={src}
+            alt="image"
+            width={1000}
+            height={1000}
+            className="absolute size-[140%] object-cover  bottom-2/5 left-1/2 transform -translate-x-1/2 translate-y-1/2 "
+          />
+        </div>
+      </motion.div>
+    </div>
+  );
+}
---
+++ app/data.ts
@@ -0,0 +1,63 @@
+// Define the type for benefit items
+export interface Benefit {
+  title: string;
+  description: string;
+  src: string;
+  color: string;
+}
+
+export const benefits: Benefit[] = [
+  {
+    title: "Easy, fun and results in just 1 minute",
+
+    description:
+      "Enjoy the time-saving gifting experience by finding the perfect gifts in just one minute!.",
+
+    src: "/images/img 01.png",
+
+    color: "#CEBDF0",
+  },
+
+  {
+    title: "Explore gifts that match your special someone (or not-so-special)",
+
+    description:
+      "Discover optios that are a perfect fit, whether for someone close or not close (like your boss)).",
+
+    src: "/images/img 02.png",
+
+    color: "#ECF97A",
+  },
+
+  {
+    title: "A special touch",
+
+    description:
+      "Carefully chosen gifts demonstrate thoughtfulness and effort, and make the recipient feel valued.",
+
+    src: "/images/img 03.png",
+
+    color: "#C7CBFE",
+  },
+  {
+    title: "Explore gifts that match your special someone (or not-so-special)",
+
+    description:
+      "Discover optios that are a perfect fit, whether for someone close or not close (like your boss)).",
+
+    src: "/images/img 04.png",
+
+    color: "#FF7D61",
+  },
+
+  {
+    title: "Make your gift more special",
+
+    description:
+      "We provide fun and unique suggestions along with our results to MAKE a difference if you want to.",
+
+    src: "/images/img 05.png",
+
+    color: "#F99FEC",
+  },
+];
